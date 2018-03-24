6f9d2176a7e71a25be62a9ce000911082b6c87d8
Simon Ip <waterloo178@yahoo.com>
2018-03-24 21:29:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,9 @@ function logShout(string)
   var string1 = string.toUpperCase();
   console.log(string1);
 }
+
+function logWhisper(string)
+{ 
+  var string2 = string.toLowerCase();
+  console.log(string2);
+}
